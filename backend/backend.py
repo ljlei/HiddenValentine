07a1b2a96a4c83b1e4d7f8b77462da1d68cd080f
Alfendi <tenzosc@uci.edu>
2023-02-04 21:42:23 -0800

    Error Checking
    Backend stuff.

--- backend/backend.py
+++ backend/backend.py
@@ -1,42 +1,36 @@
-import config, classes
+import config
 from flask import Flask, request, render_template, flash, redirect
 from twilio.rest import Client
-from backend.classes import Valentine
+from classes import Valentine
 
-# Your Account SID from twilio.com/console
-account_sid = "AC6278b718883836facee2f0615967fe89"
-# Your Auth Token from twilio.com/console
+account_sid = config.account_sid
 auth_token  = config.api_key
-
 client = Client(account_sid, auth_token)
 
-app = Flask(__name__, template_folder="templateFiles")
+app = Flask(__name__, template_folder="templateFiles", static_folder='staticFiles')
+app.config['SECRET_KEY'] = config.secret_key
 
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
     form = Valentine()
     if form.validate_on_submit():
-        pass
-    student = {'student' : 'UCI Student'}
-    return render_template('test.html', student=student)
+        send_message()
+        return redirect('/')
+    return render_template('test.html', form=form)
+
 
 @app.route("/about")
 def about():
     return "We're cool."
 
-@app.route("/send_message", methods=["POST"])
 def send_message():
-    # Retrieve data from the request
     recipient = request.form.get("recipient")
-    message = request.form.get("message")
+    valentine = request.form.get("valentine")
 
-    message = client.messages.create(
-        recipient="+19097644355",
-        from_="+19093513871",
-        message="Hello from Python!")
-
-    return "Message sent!"
+    client.messages.create(
+        to=recipient,
+        from_=config.twilio_number,
+        body=valentine)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
--- backend/classes.py
+++ backend/classes.py
@@ -1,11 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, NumberRange, Length
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 class Valentine(FlaskForm):
-    recipient = IntegerField('recipient', validators=[DataRequired(), NumberRange
-                                                     (min=0, max=9, message="Please enter a number between 1-9!"),
-                                                     Length(min=1, max=11)])
-    message = StringField('message', validators=[DataRequired(), Length(min=-1, max=1600)])
-    submit = SubmitField('submitted')
-
+    recipient = StringField('recipient', validators=[DataRequired(), Length(min=1, max=10)])
+    valentine = StringField('valentine', validators=[DataRequired(), Length(min=-1, max=1600)])
+    submit = SubmitField('Submit')
