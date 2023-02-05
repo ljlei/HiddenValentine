8edb7c6f0a98d675ba37dffa441bcae2e4ecdbdd
Alfendi <tenzosc@uci.edu>
2023-02-05 05:11:32 -0800

    Everything working? (I hope)
    Could use some code clean-up and phone number validation.

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-import config
+import config, time
 from flask import Flask, request, render_template, redirect, url_for
 from twilio.rest import Client
 from classes import Valentine
@@ -10,27 +10,42 @@ client = Client(account_sid, auth_token)
 app = Flask(__name__, template_folder="templateFiles", static_folder='staticFiles')
 app.config['SECRET_KEY'] = config.secret_key
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/home")
+@app.route("/home", methods=['GET', 'POST'])
 def main_page():
     form = Valentine()
     if form.validate_on_submit():
         send_message()
-    return render_template('test.html', form=form)
+        return redirect(url_for('confirmation'))
+    return render_template('index.html', form=form)
 
-@app.route("/about")
-def about():
-    return "We're cool."
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+@app.route("/confirmation", methods=['GET', 'POST'])
+def confirmation():
+    return render_template('confirmation.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 def send_message():
     recipient = request.form.get("recipient")
     valentine = request.form.get("valentine")
 
-    client.messages.create(
+    confirmation = client.messages.create(
         to=recipient,
         from_=config.twilio_number,
         body=valentine)
+    
+    print(confirmation)
+    
+@app.route('/', defaults={'path': '/home'})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# return redirect('/')
